94ecd0c0d21dad848bdafc5085de8a5c3845d61c
Esben Viskum <viskum@viskum.dk>
2021-06-01 12:29:34 +0200

    Initial Commit

---
+++ SignUpForm-Master/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  SignUpForm-Master
+//
+//  Created by Esben Viskum on 31/05/2021.
+//
+
+import SwiftUI
+import SignUpForm
+
+func checkUsername(username: String) -> Bool {
+    if username == "esben@viskum.dk" { return true }
+    return false
+}
+
+
+struct ContentView: View {
+    @StateObject private var signUpViewModel = SignUpViewModel(
+        completion: { username, password in
+        print("Username: \(username) Password: \(password)")
+    }, usernameValidationType: .email(checkUsername))
+
+    var body: some View {
+        NavigationView {
+            SignUpView(signUpViewModel: signUpViewModel)
+                .navigationTitle("Sign Up")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ SignUpForm-Master/SignUpForm_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpForm_MasterApp.swift
+//  SignUpForm-Master
+//
+//  Created by Esben Viskum on 31/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SignUpForm_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
